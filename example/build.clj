cd43171ffd3111423ca51f28930ff10ce8cc445b
Wanja Hentze <wanja.hentze@bevuta.com>
2022-04-27 17:21:48 +0200

    add a small-ish example clojure program

---
+++ example/build.clj
@@ -0,0 +1,24 @@
+; adapted from https://clojure.org/guides/tools_build#_compiled_uberjar_application_build
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+(def lib 'simple)
+(def version (format "1.2.%s" (b/git-count-revs nil)))
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def uber-file (format "target/uber.jar" (name lib) version))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn uber [_]
+  (clean nil)
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/compile-clj {:basis basis
+                  :src-dirs ["src"]
+                  :class-dir class-dir})
+  (b/uber {:class-dir class-dir
+           :uber-file uber-file
+           :basis basis
+           :main 'simple}))
