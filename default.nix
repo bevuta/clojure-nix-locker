737efc9e783900e12e62d2c848e14a5b81d6dd29
Moritz Heidkamp <moritz.heidkamp@bevuta.com>
2022-05-12 16:26:24 +0200

    Provide `shellEnv` and `wrapClojure` helpers
    * `shellEnv` can be sourced in scripts to make `clojure` use the locked
      dependencies
    * `wrapClojure` can be used to wrap a clojure derivation to use the
      locked dependencies

--- default.nix
+++ default.nix
@@ -24,7 +24,7 @@ in {
         "https://repo1.maven.org/maven2/"
         "https://repo.clojars.org/"
       ]
-    }: {
+    }: rec {
       commandLocker = command: pkgs.writeShellApplication {
         name = "clojure-nix-locker";
         runtimeInputs = [
@@ -65,5 +65,26 @@ in {
       homeDirectory = import ./createHome.nix {
         inherit pkgs lockfile mavenRepos;
       };
+      shellEnv = pkgs.writeTextFile {
+        name = "clojure-nix-locker.shell-env";
+        text = ''
+          export HOME="${homeDirectory}"
+          export JAVA_TOOL_OPTIONS="-Duser.home=${homeDirectory}"
+        '';
+        meta = {
+          description = ''
+            Can be sourced in shell scripts to export environment
+            variables so that `clojure` uses the locked dependencies.
+          '';
+        };
+      };
+      wrapClojure = clojure:
+        (pkgs.runCommandNoCC "locked-clojure" { buildInputs = [ pkgs.makeWrapper ]; } ''
+          mkdir -p $out/bin
+          makeWrapper ${clojure}/bin/clojure $out/bin/clojure \
+            --run "source ${shellEnv}"
+          makeWrapper ${clojure}/bin/clj $out/bin/clj \
+            --run "source ${shellEnv}"
+        '');
   };
 }
